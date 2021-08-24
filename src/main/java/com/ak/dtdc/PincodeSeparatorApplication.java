3489cfa363178b6be9548bb3ec9a125526383d19
AKandAK <akandak9@gmail.com>
2021-08-25 01:00:19 +0530

    Server and Client working good, need to improve separator algorithm

---
+++ src/main/java/com/ak/dtdc/PincodeSeparatorApplication.java
@@ -0,0 +1,22 @@
+package com.ak.dtdc;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class PincodeSeparatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PincodeSeparatorApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner init(StorageService storageService) {
+		return (args) -> {
+			storageService.deleteAll();
+			storageService.init();
+		};
+	}
+}
---
+++ src/main/java/com/ak/dtdc/separator.java
@@ -0,0 +1,148 @@
+package com.ak.dtdc;
+
+import java.io.File;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import jxl.Sheet;
+import jxl.Workbook;
+import jxl.write.Label;
+import jxl.write.Number;
+import jxl.write.WritableCell;
+import jxl.write.WritableSheet;
+import jxl.write.WritableWorkbook;
+
+public class separator {
+	
+	private static String FILEPATH = "/home/ak/Downloads/ms.xls";
+    private static int workingSheet = 0;    
+    
+    public separator(String filepath, int sheetNum) {
+        FILEPATH = filepath;
+        workingSheet = sheetNum;
+    }
+    
+    public separator(String filepath) {
+        FILEPATH = filepath;
+        workingSheet = 0;
+    }
+    
+    public static void separate(String FILEPATH) throws Exception {
+
+    	final Workbook existingBook = Workbook.getWorkbook(new File(FILEPATH));
+        final String[] sheets = existingBook.getSheetNames();
+        final Sheet sheet = existingBook.getSheet(workingSheet);
+        int baseRow = -1;
+        final int pinCol = sheet.getColumns();
+        final int phoneCol = pinCol + 1;
+        int dataCol = -1;
+        int row = -1;
+        row = 0;
+        while (true) {
+            int count = 0;
+            for (int i = 0; i < pinCol; ++i) {
+                final String st = sheet.getCell(i, row).getContents();
+                final char[] arr = st.toCharArray();
+                count = 0;
+                for (int j = 0; j < arr.length; ++j) {
+                    if (arr[j] >= '0' && arr[j] <= '9') {
+                        ++count;
+                    }
+                }
+                if (count > 10) {
+                    dataCol = i;
+                    baseRow = row - 1;
+                    break;
+                }
+            }
+            if (count > 10) {
+                break;
+            }
+            ++row;
+        }
+        WritableWorkbook writeBook = Workbook.createWorkbook(new File(FILEPATH), existingBook);
+        WritableSheet wSheet = writeBook.getSheet(workingSheet);
+        wSheet.addCell((WritableCell)new Label(pinCol, baseRow, "PINCODE"));
+        wSheet.addCell((WritableCell)new Label(phoneCol, baseRow, "PHONE"));
+        // System.out.println("process initiated...");
+        final int rows = sheet.getRows();
+        for (int k = 0; k < pinCol; ++k) {
+            wSheet.addCell((WritableCell)new Label(k, baseRow, sheet.getCell(k, baseRow).getContents()));
+        }
+        for (int l = row; l < rows; ++l) {
+            final String str = sheet.getCell(dataCol, l).getContents();
+            final String[] ans = getP(str);
+            wSheet.addCell((WritableCell)new Number(pinCol, l, (double)Integer.parseInt(ans[0])));
+            wSheet.addCell((WritableCell)new Number(phoneCol, l, (double)Long.parseLong(ans[1])));
+        }
+        existingBook.close();
+        writeBook.write();
+        writeBook.close();
+        // System.out.println("completed");
+    }
+    
+    
+    /*
+     * Returns both pincode, phone after separating
+     */
+    public static String[] getP(String str) {
+        String p = "";
+        String pin = "0";
+        String phn = "0";
+        int count = 0;
+        boolean pinStarted = false;
+        for (int i = 0; i < str.length(); ++i) {
+            final char c = str.charAt(i);
+            if (c >= '0' && c <= '9') {
+                p = String.valueOf(p) + c;
+                ++count;
+                pinStarted = true;
+            }
+            else if (pinStarted && (c == 'O' || c == 'o')) {
+                p = String.valueOf(p) + '0';
+                ++count;
+            }
+            else if (pinStarted) {
+                pinStarted = false;
+                p = "";
+                count = 0;
+            }
+            if (count == 6) {
+                if (i < str.length() - 1) {
+                    final char next = str.charAt(i + 1);
+                    if ((next < '0' || next > '9') && next != 'O') {
+                        if (next != 'o') {
+                            pin = p;
+                            count = 0;
+                            p = "";
+                            pinStarted = false;
+                        }
+                    }
+                }
+                else {
+                    pin = p;
+                    p = "";
+                    pinStarted = false;
+                }
+            }
+            else if (count == 10) {
+                if (i < str.length() - 1) {
+                    final char next = str.charAt(i + 1);
+                    if (next < '0' || next > '9') {
+                        count = 0;
+                        phn = p;
+                        p = "";
+                        pinStarted = false;
+                    }
+                }
+                else {
+                    phn = p;
+                    p = "";
+                    pinStarted = false;
+                }
+            }
+        }
+        return new String[] { pin, phn };
+    }
+}
